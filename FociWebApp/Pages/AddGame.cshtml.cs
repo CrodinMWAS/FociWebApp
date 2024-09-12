c56b4292818aafbb77f369da47b721565a9a7e48
CrodinMWAS <MWASiskola@gmail.com>
2024-09-12 09:18:00 +0200

    EndOfClass

---
+++ FociWebApp/Pages/AddGame.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using FociWebApp.Models;
+
+namespace FociWebApp.Pages
+{
+    public class AddGameModel : PageModel
+    {
+        private readonly FociWebApp.Models.SoccerDBContext _context;
+
+        public AddGameModel(FociWebApp.Models.SoccerDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+
+        [BindProperty]
+        public Game Game { get; set; } = default!;
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Games.Add(Game);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
---
+++ FociWebApp/Pages/GameList.cshtml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using FociWebApp.Models;
+
+namespace FociWebApp.Pages
+{
+    public class GameListModel : PageModel
+    {
+        private readonly FociWebApp.Models.SoccerDBContext _context;
+
+        public GameListModel(FociWebApp.Models.SoccerDBContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Game> Game { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            Game = await _context.Games.ToListAsync();
+        }
+    }
+}
